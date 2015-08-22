36ccebefc34998b4b3a8629cead8fd437d87b242
zjhmale <zjhmale@gmail.com>
2015-08-22 12:17:01 +0800

    add vim path and three rc files

---
+++ setup.sh
@@ -0,0 +1,6 @@
+cp -r vim ~/.vim
+cp vimrc ~/.vimrc
+cp vimrc_shell ~/.vimrc_shell
+cp vimrc_macvim ~/.vimrc_macvim
+
+echo "setup done"
---
+++ vimrc
@@ -0,0 +1,5 @@
+if has("gui_macvim")
+    source ~/.vimrc_macvim
+else
+    source ~/.vimrc_shell
+endif
